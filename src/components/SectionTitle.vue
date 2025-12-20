0e127a6d9c77195173b69af98aa68edd1ef44f85
Elknir <85866045+Elknir@users.noreply.github.com>
2025-12-20 18:10:06 +0100

    Title responsive

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="inline-block p-16">
+  <div class="inline-block p-4 md:p-8 lg:p-16">
     <div class="bg-white">
       <!-- Upper Content -->
-      <div class="bg-black text-white px-3 py-1 text-xs font-mono">• now entering</div>
+      <div class="bg-black text-white px-3 md:py-1 font-mono text-[11px] md:text-sm">
+        • now entering
+      </div>
 
       <!-- Lower Content -->
       <div class="flex">
         <!-- Main Text -->
-        <div class="px-8 pr-64 py-4 flex items-center">
-          <h2 class="text-6xl">
+        <div class="px-4 md:px-6 lg:px-8 md:py-2 lg:py-4 min-w-20 flex items-center">
+          <h2 class="text-2xl md:text-4xl lg:text-5xl flex-1">
             <slot></slot>
           </h2>
         </div>
 
         <!-- Color Cube -->
-        <div class="w-32" :class="bgColor"></div>
+        <div class="w-12 md:w-24 lg:w-32" :class="bgColor"></div>
       </div>
     </div>
   </div>
@@ -30,5 +32,3 @@ withDefaults(
   },
 ) // cyan par défaut
 </script>
-
-<style scoped></style>
